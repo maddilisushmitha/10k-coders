ce33fa1c9f5264cc62a64c4da0dbe890795bcc3d
maddilisushmitha <maddilisushmitha@gmail.com>
2022-12-30 17:06:47 +0530

    changesdone

--- js/HTML/dom.js
+++ js/HTML/dom.js
@@ -3,18 +3,10 @@
 
 // four methods:get,put,delete,post=>curd=>create=post,update=get,read=put,dlete=delete
 
-fetch("https://jsonplaceholder.typicode.com/posts")
-.then((response)=>
-response.json())
 
-.then((json)=>
-console.log(json));
-
-let data=[];
-
-fetch("https://jsonplaceholder.typicode.com/posts")
-.then((response)=>
-response.json())
-
-.then((json)=>DataTransfer.push(json));
-console.log(data);
+async function getData(){
+    let response=await fetch("https://jsonplaceholder.typicode.com/posts")
+    let output=await response.json();
+    console.table(output);
+}
+getData()
---
+++ js/HTML/todo.js
@@ -0,0 +1,16 @@
+let addToDoButton=document.getElementById("addToDo");
+let todocontainer=document.getElementById("todocontainer")
+let inputfield=document.getElementById("inputfield")
+addToDoButton.addEventListener('click',function(){
+    var paragraph=document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerText=inputfield.value;
+    todocontainer.appendChild(paragraph);
+    inputfield.value="";
+    paragraph.addEventListener('click',function(){
+        paragraph.style.textDecoration="line-through"
+    })
+    paragraph.addEventListener('dblclick',function(){
+        todocontainer.removeChild(paragraph)
+    })
+})
